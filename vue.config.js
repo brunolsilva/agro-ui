044c8ee2c8a14bcb499ccba62eed5c0adf411141
Bruno Luiz da Silva <brunolsilva@users.noreply.github.com>
2021-10-05 15:03:31 -0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
     baseUrl: '/batata/'
 }
-
-https://servicos-aceiteinty.tokiomarine.com.br/ems/act/Aceitacao/mvc/aceitacaoRest/liberacaoDIV/4232866/PLT/1317/373/999999
-https://servicos-aceiteinty.tokiomarine.com.br/ems/act/Aceitacao/mvc/aceitacaoRest/liberacaoDIV/4232866/PLT/1317/373/999999
